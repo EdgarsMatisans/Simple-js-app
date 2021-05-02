e3d546b1a651032a7a021fdc649fa9733d603e2a
EdgarsMatisans <mcl92805@gmail.com>
2021-05-02 20:35:26 +0200

    pokemonlist
    created pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ let myName = 'Edgars';
 document.write(myName);
 myName = 'Matisans';
 document.write(myName);
+
+let pokemonList = [{name: "Bulbasaur", height: 0,7, types ['grass', 'poison']}
+                   {name: "Ivysaur", height: 1, types ['grass', 'poison']}
+                   {name: "Venusaur", height: 2, types ['grass', 'poison']}]
