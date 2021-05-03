acab4e7749bfa9d8a1b9728427763ef885f318ce
EdgarsMatisans <mcl92805@gmail.com>
2021-05-03 21:59:50 +0200

    beauty
    Made looks better

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,5 @@ myName = "Matisans";
 document.write(myName);
 
 let PokemonList = [{ name: "Bulbasaur", height: 0.7, types: ["grass", "poison"], },
-    { name: "Ivysaur", height: 1, types: ["grass", "poison"], },
-    { name: "Venusaur", height: 2, types: ["grass", "poison"], },
-
-]
+                   { name: "Ivysaur", height: 1, types: ["grass", "poison"], },
+                   { name: "Venusaur", height: 2, types: ["grass", "poison"], },]
