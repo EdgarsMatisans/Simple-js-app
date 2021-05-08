214adea9c6e56e714dfee3a94c4fe90dfa26db43
EdgarsMatisans <mcl92805@gmail.com>
2021-05-08 09:40:05 +0200

    let pokemonlist
    moved down to make looks better
    added loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,18 @@ document.write(myName);
 myName = "Matisans";
 document.write(myName);
 
-let PokemonList = [{ name: "Bulbasaur", height: 0.7, types: ["grass", "poison"], },
-                   { name: "Ivysaur", height: 1, types: ["grass", "poison"], },
-                   { name: "Venusaur", height: 2, types: ["grass", "poison"], },]
+let PokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
+    { name: "Venusaur", height: 2, types: ["grass", "poison"] },
+];
+
+for (let i = 0; i < PokemonList.length; i++) {
+    if (PokemonList[i].height > 1.5) {
+        console.log(PokemonList[i].name + " Wow, that’s big!");
+    } else if (PokemonList[i].height <= 1.5 && PokemonList[i].height > 0.8) {
+        console.log(PokemonList[i].name + " Am, middle size!");
+    } else if (PokemonList[i].height < 0.8) {
+        console.log(PokemonList[i].name + " He is small!");
+    }
+}
