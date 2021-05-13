2a165f8d56f86fd8a32211d715d41484219398be
EdgarsMatisans <mcl92805@gmail.com>
2021-05-13 20:18:44 +0200

    updates
    command for was changed for forEach
    was added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,33 @@ document.write(myName);
 myName = "Matisans";
 document.write(myName);
 
-let PokemonList = [
-    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-    { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
-    { name: "Venusaur", height: 2, types: ["grass", "poison"] },
-];
+var PokemonRepository = function() {
+    var PokemonList = [
+        { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+        { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
+        { name: "Venusaur", height: 2, types: ["grass", "poison"] },
+    ];
 
-for (let i = 0; i < PokemonList.length; i++) {
-    if (PokemonList[i].height > 1.5) {
-        document.write(PokemonList[i].name + " Wow, that’s big!");
-    } else if (PokemonList[i].height <= 1.5 && PokemonList[i].height > 0.8) {
-        document.write(PokemonList[i].name + " Am, middle size!");
-    } else if (PokemonList[i].height < 0.8) {
-        document.write(PokemonList[i].name + " He is small!");
+    function getAll() {
+        return PokemonList;
     }
-}
+
+    function add(pokemon) {
+        PokemonList.push(pokemon);
+    }
+    return {
+        getAll: getAll,
+        add: add,
+    };
+    console.log(PokemonRepository.getAll());
+
+    PokemonList.forEach(function(pokemon) {
+        if (PokemonList[i].height > 1.5) {
+            document.write(PokemonList[i].name + " Wow, that’s big!");
+        } else if (PokemonList[i].height <= 1.5 && PokemonList[i].height > 0.8) {
+            document.write(PokemonList[i].name + " Am, middle size!");
+        } else if (PokemonList[i].height < 0.8) {
+            document.write(PokemonList[i].name + " He is small!");
+        }
+    });
+};
